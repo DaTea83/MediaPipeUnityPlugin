04be98a1739cc6703df3e98f21f0826956d61c92
Eugene C <eugenechoy83@gmail.com>
2026-02-21 23:07:30 +0800

    Add grid spawn

--- Assets/ChoyUtilities/Scripts/Entities/Spawn/TextGridSpawnAuthoring.cs
+++ Assets/ChoyUtilities/Scripts/Entities/Spawn/TextGridSpawnAuthoring.cs
@@ -1,6 +1,8 @@
-﻿using Unity.Collections;
+﻿using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace EugeneC.ECS
@@ -11,6 +13,7 @@ namespace EugeneC.ECS
 		[SerializeField] private GameObject[] prefabs;
 		[SerializeField] private char[] identifiers;
 		[SerializeField] private TextAsset textAsset;
+		[SerializeField] private int2 total = new int2(10, 10);
 		[SerializeField] private float2 spacing = new float2(1f, 1f);
 		[SerializeField] private float scale = 1f;
 		
@@ -18,6 +21,7 @@ namespace EugeneC.ECS
 		{
 			public override void Bake(TextGridSpawnAuthoring authoring)
 			{
+				DependsOn(authoring.textAsset);
 				var e = GetEntity(TransformUsageFlags.Dynamic);
 
 				var builder = new BlobBuilder(Allocator.Temp);
@@ -29,24 +33,27 @@ namespace EugeneC.ECS
 				{
 					Pattern = pattern,
 					Scale = authoring.scale,
-					Spacing = authoring.spacing
+					Spacing = authoring.spacing,
+					Total = authoring.total
 				});
 
 				var buffer = AddBuffer<TextGridIBuffer>(e);
 				for (ushort i = 0; i < authoring.prefabs.Length; i++)
 				{
+					if (authoring.prefabs[i] is null)
+						throw new System.ArgumentException();
+					
 					var p = GetEntity(authoring.prefabs[i], TransformUsageFlags.Dynamic);
 					buffer.Add(new TextGridIBuffer
 					{
-						Id = i,
 						Prefab = p
 					});
 				}
 			}
 
-			private static BlobAssetReference<BlobArray<FixedList512Bytes<byte>>> MakeBlob(TextGridSpawnAuthoring authoring, ref BlobBuilder builder)
+			private static BlobAssetReference<BlobArray<FixedList512Bytes<byte>>> MakeBlob(TextGridSpawnAuthoring authoring, 
+				ref BlobBuilder builder)
 			{
-				builder = new BlobBuilder(Allocator.Temp);
 				ref var patternBlob = ref builder.ConstructRoot<BlobArray<FixedList512Bytes<byte>>>();
 				
 				var arrayBuilder = builder.Allocate(ref patternBlob, 1);
@@ -58,15 +65,12 @@ namespace EugeneC.ECS
 				{
 					if(char.IsControl(c)) continue;
 					if(c == '\n') continue;
-
-					for (var i = 0; i < patternId.Length; i++)
+					
+					for (byte i = 0; i < patternId.Length; i++)
 					{
 						if (patternId[i] != c) continue;
-						
-						var patternByte = (byte)i;
-						patternList.Add(patternByte);
+						patternList.Add(i);
 					}
-					
 				}
 
 				arrayBuilder[0] = patternList;
@@ -80,21 +84,56 @@ namespace EugeneC.ECS
 		public BlobAssetReference<BlobArray<FixedList512Bytes<byte>>> Pattern;
 		public float2 Spacing;
 		public float Scale;
+		public int2 Total;
 	}
 
 	public struct TextGridIBuffer : IBufferElementData
 	{
-		public ushort Id;
 		public Entity Prefab;
 	}
 
+	[BurstCompile]
 	[UpdateInGroup(typeof(Eu_InitializationSystemGroup), OrderFirst = true)]
 	[UpdateAfter(typeof(InitializeRandomISystem))]
 	public partial struct TextGridSpawnISystem : ISystem
 	{
+		[BurstCompile]
 		public void OnUpdate(ref SystemState state)
 		{
+			var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
 			
+			foreach (var (spawn, buffer, ltw, entity) 
+			         in SystemAPI.Query<RefRO<TextGridSpawnIData>, DynamicBuffer<TextGridIBuffer>, RefRO<LocalToWorld>>()
+				         .WithAll<TextGridSpawnIData>().WithEntityAccess())
+			{
+				var startPos = ltw.ValueRO.Position;
+				var pattern = spawn.ValueRO.Pattern.Value[0];
+
+				var spawnIndex = 0;
+				for (var x = 0; x < spawn.ValueRO.Total.x; x++)
+				{
+					for (var y = 0; y < spawn.ValueRO.Total.y; y++, spawnIndex++)
+					{
+						var spawnType = pattern[spawnIndex];
+						var spawnEntity = buffer[spawnType].Prefab;
+						if (spawnEntity == Entity.Null) continue;
+						
+						var newEntity = ecb.Instantiate(spawnEntity);
+						var eLt = LocalTransform.FromPositionRotationScale(
+							new float3
+						{
+							x = startPos.x + x * spawn.ValueRO.Spacing.x,
+							y = startPos.y,
+							z = startPos.z + y * spawn.ValueRO.Spacing.y
+						}, 
+							quaternion.identity, 
+							spawn.ValueRO.Scale);
+						ecb.SetComponent(newEntity, eLt);
+					}
+				}
+				ecb.RemoveComponent<TextGridSpawnIData>(entity);
+			}
+			ecb.Playback(state.EntityManager);
 		}
 	}
 }
