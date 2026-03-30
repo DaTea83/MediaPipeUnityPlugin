4b6a351e24109cf80e0ced30637027739fb2308c
Eugene C <eugenechoy83@gmail.com>
2026-03-30 16:11:12 +0800

    Added raw proto files

---
+++ Packages/com.datea83.mediapipefordots/Runtime/Protobuf/Gpu/GlTextureInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MediaPipeForDOTS {
+    
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GlTextureInfo {
+        
+        public int glInternalFormat;
+        public uint glFormat;
+        public uint glType;
+        public int downscale;
+    }
+}
